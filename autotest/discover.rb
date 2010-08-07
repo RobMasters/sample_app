d1a54df7d19ec629c9be96829a249b64ee6e954e
Rob Masters <mastahuk@gmail.com>
2010-08-08 00:30:19 +0100

    layout

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,2 +1,6 @@
 Autotest.add_discovery { "rails" }
 Autotest.add_discovery { "rspec2" }
+
+Autotest.add_hook :initialize do |autotest| autotest.add_mapping(/ˆspec\/requests\/.*_spec\.rb$/) do
+  autotest.files_matching(/ˆspec\/requests\/.*_spec\.rb$/) end
+end
